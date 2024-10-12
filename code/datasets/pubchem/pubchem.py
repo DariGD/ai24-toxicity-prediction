ce792a9cfb974a13619b0a0392df14dcf8a6233f
Yury Konov <gkonoff@gmail.com>
2024-10-13 02:07:45 +0300

    add: dataset scraping code version 0.1.0 (stable)

--- code/datasets/pubchem/pubchem.py
+++ code/datasets/pubchem/pubchem.py
@@ -1,48 +1,74 @@
-# TODO: create module to collect data from PubChem
-#       destination - ???
-import requests
+# Module 'pubchem'
+# Utilities to collect data from PubChem APIs
+
 import pprint
 import json
+import httpx
 
 
 base_PUG_REST = "https://pubchem.ncbi.nlm.nih.gov/rest/pug"
 base_PUG_VIEW = "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view"
 
 
-# function to make HTTP GET call to https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/smiles/CCC/CIDs/json,
-# get response in JSON format like this:
-# {
-#   "IdentifierList": {
-#     "CID": [
-#       6334
-#     ]
-#   }
-# }
-# and extract value of the CID property
 def smiles_to_cid(smiles):
-    response = requests.get(f"{base_PUG_REST}/compound/smiles/{smiles}/CIDs/json")
+    '''
+    Convert SMILES to CID by
+    making HTTP GET call to PUG_REST API
+    :param
+        smiles: SMILES string
+    :return:
+        CID of the compound
+    '''
+    response = httpx.get(f"{base_PUG_REST}/compound/smiles/{smiles}/CIDs/json")
+    # Response structure
+    # {
+    #   "IdentifierList": {
+    #     "CID": [
+    #       6334
+    #     ]
+    #   }
+    # }
     return response.json()["IdentifierList"]["CID"][0]
 
 
-# function to make HTTP GET call to https://pubchem.ncbi.nlm.nih.gov/rest/pug_view//data/compound/{CID}/JSON?heading=Toxicity+Data
-def get_toxicity_data(cid):
-    response = requests.get(f"{base_PUG_VIEW}/data/compound/{cid}/JSON?heading=Toxicity+Data")
-    return response.json()
+def get_toxicity_data(cid, full_toxicity_data=False):
+    '''
+    Get toxicity data from PUG_VIEW API.
+    Applies additional filtering to exctact only
+    the "Toxicity Data" section from the response.
+    :param
+        cid: CID of the compound
+    :return:
+        JSON response from PubChem API
+    '''
+    response = httpx.get(
+        f"{base_PUG_VIEW}/data/compound/{cid}/JSON",
+        params=f"heading=Toxicity{'' if full_toxicity_data else '+Data'}"
+    )
+    return response
 
 
-# function to extract
-# .Record.Section[0].Section[0].Section[0].Information[0].Value.StringWithMarkup[0].String
-# from JSON response
 def get_toxicity_data_value(json_response):
-    return json_response["Record"]["Section"][0]["Section"][0]["Section"][0]["Information"][0]["Value"]["StringWithMarkup"][0]["String"]
+    '''
+    Extracts value from JSON response.
+    Usually the value is located at
+    `.Record.Section[0].Section[0].Section[0].Information[0].Value.StringWithMarkup[0].String`
+    of the response JSON.
+    :param
+        json_response: JSON response from PUG_VIEW API
+    :return:
+        Value (string) from JSON response
+    '''
+    return json_response["Record"]["Section"][0]["Section"][0][
+        "Section"
+    ][0]["Information"][0]["Value"]["StringWithMarkup"][0]["String"]
 
 
-smiles = input("Smiles: ")
-cid = smiles_to_cid(smiles)
-
-
-print(f"CID: {cid}")
-toxicity_data = get_toxicity_data(cid)
-json.dump(toxicity_data, open("toxicity.json", "w"), indent=4)
-pprint.pp(toxicity_data)
-pprint.pp(get_toxicity_data_value(toxicity_data))
+if __name__ == "__main__":
+    smiles = input("Smiles: ")
+    cid = smiles_to_cid(smiles)
+    print(f"CID: {cid}")
+    toxicity_data = get_toxicity_data(cid)
+    json.dump(toxicity_data, open("toxicity.json", "w"), indent=4)
+    pprint.pp(toxicity_data)
+    pprint.pp(get_toxicity_data_value(toxicity_data))
